970f6388a8918bec5706679cb20ba7953cadc657
Hanzala Afzal-pc2 <malikhunzala1337@gmail.com>
2022-04-25 00:43:32 +0500

    mac support added

--- app/Http/Controllers/Api/Authentication.php
+++ app/Http/Controllers/Api/Authentication.php
@@ -7,21 +7,48 @@ use Illuminate\Http\Request;
 use Validator;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use JWTAuth;
+use DB;
 
 class Authentication extends Controller
 {
+
+    private function checkMacAddress($number,$mac){
+        $data=DB::table('users')->where('number',$number)->get()->toArray();
+        if($data[0]->mac_address==null || empty($data[0]->mac_address)){
+          DB::table('users')->where('number',$number)->update([
+            'mac_address' => $mac
+          ]);
+          return true;
+        }else{
+          //match mac address
+          if($data[0]->mac_address==$mac){
+            return true;
+          }else{
+            return false;
+          }
+        }
+    }
+
     public function authenticate(Request $req){
       $data=$req->only('number','password');
-      $validator=Validator::make($data,[
+      $validator=Validator::make($req->all(),[
         'number' => 'required|exists:users,number',
         'password' => 'required',
+        'mac' => 'required'
       ],[
         'number.required' => 'number is required',
         'password.required' => 'password is required',
         'number.exists' => 'Invalid Number',
+        'mac.required' => 'mac address required'
       ]);
       if ($validator->fails()) {
           return response()->json(['error' => $validator->messages()], 500);
+      }
+
+      if($this->checkMacAddress($req->number,$req->mac)==false){
+        return response()->json([
+          'error' => 'Mac address not matched'
+        ],500);
       }
 
       try {
